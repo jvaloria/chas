fe9a3f464b91380b47dc5db6ffd95409169d4a10
MartinPustel <martinpustelnik@gmail.com>
2025-05-30 20:57:17 -0300

    Escena Guada

---
+++ Assets/Guiones/Guada.ink
@@ -0,0 +1,18 @@
+EXTERNAL LoadObject(string object)
+EXTERNAL RemoveObject(string object)
+EXTERNAL TimedOption(int optionNumber, int timeForOption)
+EXTERNAL LoadScreen(int screenNumber)
+EXTERNAL PlaySound(string soundToPlay)
+ INCLUDE GLOBALS.ink
+
+
+->main
+===main===
+Hola como estas?
+
+    *bien -> guada2
+    *mal -> guada2
+    *ok ->DONE
+===guada2===
+    guada2
+->DONE
---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private bool playsSound;
+    [SerializeField] private bool hasCustomAction;
+    [SerializeField] private bool isUsedOnce;
+    [SerializeField] private AudioSource objectAudio;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onInteraction()
+    {
+        if (isUsedOnce)
+        {
+            this.gameObject.SetActive(false);
+        }
+        if (playsSound)
+        {
+            Debug.Log("play interactable object sound");
+            //SoundManager.play(objectAudio);
+        }
+
+    }
+
+    public virtual void CustomAction()
+    {
+        Debug.Log("custom action override method not implemented");
+        return;
+    }
+
+    
+}
